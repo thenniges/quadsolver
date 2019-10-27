44b4c3e0a482cec2bd93a866723164162b21f3af
Skyler Sheler <skyler.j.sheler@wmich.edu>
2019-10-27 16:03:03 -0400

    error checking established and concatenation process completed

--- src/format/format.c
+++ src/format/format.c
@@ -9,8 +9,58 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 */
 
 #include "format.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 int format(double result1, double result2, char* output)
 {
+	if(isnormal(result1)<0 || isnormal(result2) < 0){
+		return-1;
+	}
+	char  result1String[80];
+	char  result2String[80];
+	char * startOfOutput = "The roots of this function are ";
+	if(isnan(result1)!=0){
+		printf("nan");
+		strcpy(result1String, "nan ");
+	}
+	else if(isinf(result1)!=0){
+		printf("inf");
+		strcpy(result1String, "inf ");
+	}
+	else{
+		printf("real number");
+		gcvt(result1, 5, result1String);
+		strcat(result1String, " ");
+	}
+	if(isnan(result2)!=0){
+		printf("nan");
+		strcpy(result2String, "nan.");
+	}
+	else if(isinf(result2)!=0){
+		printf("inf");
+		strcpy(result2String, "inf.");
+	}
+	else{
+		printf("real number");
+		gcvt(result2, 5, result2String);
+		strcat(result2String, ".");
+	}
+	strcpy(output, startOfOutput);
+	strcat(output, result1String);
+	strcat(output, "and ");
+	strcat(output, result2String);
+	// strcat(output, result1String);
+	// strcat(output, result2String);
 	return 1;
 }
+
+// int main(int argc, char* argv[]){
+// 	char output[62];
+// 	// output[0] = '\0';
+// 	format(INFINITY, NAN, output);
+// 	printf("\n%s",output);
+// 	return 1;
+// }
