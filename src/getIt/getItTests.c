4fb7bde3ef9fd49b83c4ef6c3f30aa5b53a42358
sysch <sysch@sdf.org>
2019-10-27 15:32:24 -0400

    stubbed individual tests for getIt()

--- src/getIt/getItTests.c
+++ src/getIt/getItTests.c
@@ -13,6 +13,26 @@ void tearDown(void) {
     //This will run at the end of every test so you can do common clean up here
 }
 
+void empty_input(void) {
+  // nothing but eol
+}
+
+void zero_length_input(void) {
+  // just /0
+}
+
+void big_input(void) {
+  // inputs that will be truncated
+}
+
+void giant_input(void) {
+  // stuff a novel in there, see what happens
+}
+
+void simple_float_input(void) {
+  // generate some actual formatted floats and run them through
+}
+
 // not needed when using generate_test_runner.rb
 int main(void) {
     UNITY_BEGIN();
