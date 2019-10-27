e50b50bf018a665944499b5449a611e5327325c8
Skyler Sheler <skyler.j.sheler@wmich.edu>
2019-10-27 17:50:17 -0400

    Format has been finished, it accepts infs, nans, and real numbers and creates the ouput message from supplied information from calculate.

--- src/format/format.c
+++ src/format/format.c
@@ -8,6 +8,7 @@ The above copyright notice and this permission notice shall be included in all c
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+#define _DEFAULT_SOURCE
 #include "format.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -16,51 +17,57 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 
 int format(double result1, double result2, char* output)
 {
-	if(isnormal(result1)<0 || isnormal(result2) < 0){
-		return-1;
-	}
+	
 	char  result1String[80];
 	char  result2String[80];
-	char * startOfOutput = "The roots of this function are ";
+	// char * startOfOutput = ;
 	if(isnan(result1)!=0){
-		printf("nan");
-		strcpy(result1String, "nan ");
+		// printf("nan");
+		strncpy(result1String, "nan ", 5);
 	}
 	else if(isinf(result1)!=0){
-		printf("inf");
-		strcpy(result1String, "inf ");
+		// printf("inf");
+		strncpy(result1String, "inf ", 5);
 	}
 	else{
-		printf("real number");
+		// printf("real number");
 		gcvt(result1, 5, result1String);
-		strcat(result1String, " ");
+		strncat(result1String, " ", 2);
 	}
 	if(isnan(result2)!=0){
-		printf("nan");
-		strcpy(result2String, "nan.");
+		// printf("nan");
+		strncpy(result2String, "nan.", 5);
 	}
 	else if(isinf(result2)!=0){
-		printf("inf");
-		strcpy(result2String, "inf.");
+		// printf("inf");
+		strncpy(result2String, "inf.", 5);
 	}
 	else{
-		printf("real number");
+		// printf("real number");
 		gcvt(result2, 5, result2String);
-		strcat(result2String, ".");
+		strncat(result2String, ".", 2);
 	}
-	strcpy(output, startOfOutput);
-	strcat(output, result1String);
-	strcat(output, "and ");
-	strcat(output, result2String);
+	// char* startOfOutput = "The roots of this function are ";
+	// int i;
+	// for(i = 0 ; i < strlen(startOfOutput) ;i++){
+	// 	output[i] = startOfOutput[i];
+	// }output[i] = '\0';
+
+	strncpy(output, "The roots of this function are ", 32);
+	strncat(output, result1String, sizeof(result1String));
+	
+	strncat(output, "and ", 5);
+	strncat(output, result2String, sizeof(result2String));
+	
 	// strcat(output, result1String);
 	// strcat(output, result2String);
 	return 1;
 }
 
 // int main(int argc, char* argv[]){
-// 	char output[62];
+// 	char *output = calloc(62, sizeof(char));
 // 	// output[0] = '\0';
-// 	format(INFINITY, NAN, output);
+// 	format(INFINITY, .123, output);
 // 	printf("\n%s",output);
 // 	return 1;
 // }
--- src/format/formatTests.c
+++ src/format/formatTests.c
@@ -19,14 +19,14 @@ void simple_output_message(void) {
     double result2 = 2.001;
     double delta = 100.0 * DBL_EPSILON;
     int flag = 0;
-    char* actual = NULL;
-    char* expected = "The roots of this function are 1.001 and 2.001";
+    char actual[46];
+    char * expected = "The roots of this function are 1.001 and 2.001.";
     //Call function
     flag = format(result1, result2, actual);
     TEST_ASSERT_EQUAL_INT(1, flag);
     TEST_ASSERT_FLOAT_WITHIN(delta, 1.001, result1);
     TEST_ASSERT_FLOAT_WITHIN(delta, 2.001, result2);
-    TEST_ASSERT_EQUAL_STRING_ARRAY(expected, actual, 46);
+    TEST_ASSERT_EQUAL_STRING(expected, actual);
 }
 
 // not needed when using generate_test_runner.rb
