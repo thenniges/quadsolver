fb64907f9fe92f9f9f177d8215ca0836a9fc4e0b
Skyler Sheler <skyler.j.sheler@wmich.edu>
2019-11-06 22:37:41 -0500

    Erron's logging spike was added to the repo

---
+++ spikes/logQuad/log.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "logger.h"
+#include <time.h>
+#include <string.h>
+#include <math.h>
+#define TAG " TEST_STATUS  "
+char *logger(const char *tag, const char *message);
+// function takes in an integer an return success or failure print to a text log.
+int main(int argc, char *argv[])
+{
+    char *message;
+    char *total;
+    int ctn = 0;
+    int ok, lp;
+
+    printf("%d", lp);
+    switch (lp)
+    {
+    case 0:
+        message = malloc(sizeof(char) * 10);
+        strcpy(message, "success\t");
+        logger(TAG, message);
+        break;
+    default:
+        message = malloc(sizeof(char) * 10);
+        strcpy(message, "fail\t");
+        logger(TAG, message);
+        break;
+    }
+    return 0;
+}
+
+char *logger(const char *tag, const char *message)
+{
+    FILE *f;
+    int count = 0;
+    time_t now;
+    char *hold[100];// this array stores logs messages to 100
+    char *result = malloc(strlen(tag) + strlen(message) + 1);
+
+    time(&now);
+    strcpy(result, TAG);
+    strcat(result, message);
+    strcat(result, (char *)ctime(&now));
+    hold[count] = result;
+
+    count++;
+    f = fopen("logs.txt", "a+");
+    if (f != NULL)
+       
+    fprintf(f, result, 1);
+
+    return result;
+}
---
+++ spikes/logQuad/logger.h
@@ -0,0 +1,5 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+char* logger(const char* tag, const char* message);
+#endif /* LOG_H */
